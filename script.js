3c25aa56f6f0e656cc4b3cd2ae8312fad407055f
Shaima <shaima.anvar@gmail.com>
2020-12-09 02:03:21 +0530

    form submission errors

--- script.js
+++ script.js
@@ -25,3 +25,21 @@ $(document).ready(function(){
 
     })
 })
+
+  ("#submit-form").submit((e)=>{
+      e.preventDefault()
+      $.ajax({
+          url:"https://script.google.com/macros/s/AKfycbxBMVY7c2YzdpZUVBmKKrP16Tzr0J5ybW7P8bd28A/exec",
+          data:$("#submit-form").serialize(),
+          method:"post",
+          success:function (response){
+              alert("Form submitted successfully")
+              window.location.reload()
+              //window.location.href="https://google.com"
+          },
+          error:function (err){
+              alert("Something Error")
+
+          }
+      })
+  })
